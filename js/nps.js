931459c4e1b7ee580ae497de3699a3096e6fb07b
Maani Beigy <manibeygi@gmail.com>
2022-07-03 16:41:22 +0430

    :zap: major update

---
+++ js/nps.js
@@ -0,0 +1,91 @@
+var dom = document.getElementById("container");
+var myChart = echarts.init(dom, null, {
+    renderer: "canvas",
+    useDirtyRect: false
+});
+var app = {};
+
+var option;
+option = {
+    grid: {
+        show: false,
+        containLabel: true,
+    },
+    tooltip: {
+        show: false,
+    },
+    yAxis: [
+        {
+            offset: 0,
+            type: "category",
+            data: [""],
+            axisLabel: {
+                margin: 0,
+            },
+            splitLine: {
+                show: false
+            },
+            axisLine: {
+                show: false
+            },
+            axisTick: {
+                show: false
+            }
+        },
+    ],
+    xAxis: {
+        show: true,
+        type: "value",
+        min: 1,
+        max: 9,
+        interval: 2,
+        axisLabel: {
+            show: true,
+            color: "#545454",
+        },
+        splitLine: {
+            show: false
+        },
+    },
+
+    series: [
+        {
+            data: [
+                {
+                    value: 7.1,
+                    itemStyle: {
+                        opacity: 1,
+                        color: "#05944F"
+                    }
+                },
+            ],
+            label: {
+                show: true,
+                color: "#545454",
+                offset: [10, -30],
+                position: "insideRight",
+            },
+            showBackground: true,
+            backgroundStyle: {
+                color: "rgba(238, 238, 238)",
+                borderRadius: [0, 10, 10, 0],
+            },
+            type: "bar",
+            barCategoryGap: "50%",
+            itemStyle: {
+                emphasis: {
+                    barBorderRadius: [10, 10]
+                },
+                normal: {
+                    barBorderRadius: [10, 10, 10, 10]
+                }
+            }
+        }
+    ]
+};
+
+if (option && typeof option === "object") {
+    myChart.setOption(option);
+}
+
+window.addEventListener("resize", myChart.resize);
