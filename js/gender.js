56a4816eeca24e357de71271cb1937c5988ca0d7
Maani Beigy <manibeygi@gmail.com>
2022-07-04 23:57:33 +0430

    :lipstick: editing yAxis offset for Gender chart

--- js/gender.js
+++ js/gender.js
@@ -10,8 +10,8 @@ var option;
 option = {
     grid: {
         show: false,
-        top: 0,
-        bottom: 0,
+        top: 30,
+        bottom: 30,
         containLabel: true,
     },
     tooltip: {
@@ -29,7 +29,7 @@ option = {
             type: "category",
             data: ["Non-binary", "Male", "Female"],
             axisLabel: {
-                margin: 10,
+                margin: 20,
                 color: "#363636",
                 align: "left",
             },
